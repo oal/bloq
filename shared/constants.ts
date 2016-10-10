0e0fd91e377874b09722a4a7be15711d054830a9
Olav Lindekleiv <olav@lindekleiv.com>
2016-10-10 16:00:48 +0200

    Or so I thought... Tweak jump height.

--- shared/constants.ts
+++ shared/constants.ts
@@ -59,6 +59,6 @@ export const enum ActionId {
 }
 
 export const PlayerSpeed = 6;
-export const PlayerJumpVelocity = 20;
+export const PlayerJumpVelocity = 15;
 export const Gravity = 90;
 export const TerminalVelocity = 45;
